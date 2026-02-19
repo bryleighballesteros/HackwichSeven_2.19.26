5795f7205d14cf0090174c9e0887d8dfc7cfeda8
Bryleigh Ballesteros <bab3@hawaii.edu>
2026-02-19 12:22:12 -1000

    In-class portion completed -need to debug, build fails

---
+++ HackwichSeven_2.19.26/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  HackwichSeven_2.19.26
+//
+//  Created by Bryleigh Ballesteros on 2/19/26.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
